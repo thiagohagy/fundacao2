3e7cf7eddbc089f185e8f84f89364097c50da1b2
Thiago Hagy <thiagohagy@hotmail.com>
2020-03-12 17:39:50 -0300

    ajuste nav

--- frontend/src/components/shared/Nav.vue
+++ frontend/src/components/shared/Nav.vue
@@ -48,14 +48,6 @@
         </b-nav-item-dropdown>
       </b-navbar-nav>
 
-      <!--
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-      -->
-
-
       <b-navbar-nav class="ml-auto ">
         <b-nav-item id="profileDropdown">
           <b-nav-item-dropdown class="nav-item" :text="decoded.name || decoded.login" right>
